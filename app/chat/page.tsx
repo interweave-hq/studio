93f08feadfd88a638ac48faa5a179d1cf1c905c3
Mike Carbone <mfcbone2@gmail.com>
2023-09-07 21:47:52 -0400

    :rocket: Adds discord chat redirect

---
+++ app/chat/page.tsx
@@ -0,0 +1,8 @@
+import { redirect } from "next/navigation";
+import { getMetadata } from "@/lib/metadata";
+
+export const metadata = getMetadata({ title: "Join The Discord" });
+
+export default async function Chat() {
+	redirect("https://discord.gg/ypfzHSJ9jU");
+}
