2fb128f22d8a98c911567fa74bf4bffe5c8fd071
Mike Carbone <mfcbone2@gmail.com>
2023-10-10 19:29:25 -0400

    :lipstick: Better footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,9 +5,6 @@ export function Footer() {
     return (
         <footer className={styles.footer}>
             <Logo __cssFor={{ root: styles.footer__logo }} />
-            <p className={styles.footer__copy}>
-                <a href="https://carbonology.in">Carbonology Interactive LLC {new Date().getFullYear()} &copy;</a>
-            </p>
             <div className={styles.footer__links}>
                 <a
                     href="https://docs.interwv.com"
@@ -56,12 +53,13 @@ export function Footer() {
                     target="_blank"
                     rel="noopener noreferrer"
                     className={styles["footer__icon-link"]}
+                    aria-label="Visit the Interweave profile on X"
                 >
                     <svg
                         viewBox="0 0 1200 1227"
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
-                        className={styles["footer__icon"]}
+                        className={`${styles["footer__icon"]} ${styles["footer__icon--x"]}`}
                     >
                         <path
                             d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
@@ -74,6 +72,7 @@ export function Footer() {
                     target="_blank"
                     rel="noopener noreferrer"
                     className={styles["footer__icon-link"]}
+                    aria-label="Visit the Interweave profile on Twitter"
                 >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -111,6 +110,15 @@ export function Footer() {
                     </svg>
                 </a>
             </div>
+            <p className={styles.footer__copy}>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://carbonology.in"
+                >
+                    Carbonology Interactive LLC {new Date().getFullYear()} &copy;
+                </a>
+            </p>
         </footer>
     );
 }
