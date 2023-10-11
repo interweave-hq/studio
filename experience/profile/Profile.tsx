b26b56aad714e77d3c6a99fcf331b51906e35e8a
Mike Carbone <mfcbone2@gmail.com>
2023-10-11 15:34:01 -0400

    :rocket: Better profile page

--- experience/profile/Profile.tsx
+++ experience/profile/Profile.tsx
@@ -3,6 +3,7 @@ import { serverRequest } from "@/lib/api/serverRequest";
 import { LogOutButton } from "./LogOutButton";
 import { Table } from "@/components";
 import styles from "./styles.module.css";
+import { getButtonStyle } from "@/components/Button";
 
 export async function Profile() {
     const { data: user } = await getProfile();
@@ -14,24 +15,34 @@ export async function Profile() {
                 <h1 className={styles.section__title}>Account</h1>
                 <p className={styles.section__text}>{user.display_name}</p>
                 <p className={styles.section__text}>{user.email}</p>
-                <Image
-                    src={user.picture}
-                    alt="profile picture"
-                    height={50}
-                    width={50}
-                />
+                {user?.picture ? (
+                    <Image
+                        src={user.picture}
+                        alt="profile picture"
+                        height={50}
+                        width={50}
+                        className={styles.profile__picture}
+                    />
+                ) : null}
             </section>
             <section className={styles.section}>
                 <h2 className={styles.section__title}>Usage</h2>
-                <p className={styles.section__text}>This billing cycle, you&apos;re responsible for {user.billing_cycle_owned_requests} requests.</p>
-                <p className={styles.section__text}>We send an invoice at the first of every month, your first month free.</p>
+                <p className={styles.section__text}>
+                    This billing cycle, you&apos;re responsible for{" "}
+                    <strong>{new Intl.NumberFormat().format(user.billing_cycle_owned_requests)} requests</strong>.
+                </p>
+                <p className={styles.section__text}>We&apos;ll send an invoice at the first of every month.</p>
+                <a
+                    className={getButtonStyle()}
+                    href="https://docs.interwv.com/pricing"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
+                    View Pricing Details
+                </a>
             </section>
             <section className={styles.section}>
                 <h2 className={styles.section__title}>Request history</h2>
-                <p className={styles.section__text}>
-                    If an interface in one of your projects has the &quot;InterfaceOwner&quot; billing strategy enabled, you may see requests from other users
-                    here.
-                </p>
                 <Table
                     columns={[
                         { header: "Time", accessorKey: "time", id: "time" },
@@ -59,6 +70,18 @@ export async function Profile() {
                     supplementalInfo={[`Showing ${entries.length} of ${user.total_owned_requests} records`]}
                     data={entries}
                 />
+                <a
+                    className={`${getButtonStyle()} ${styles["billing__details-button"]}`}
+                    href="https://docs.interwv.com/specification/billing-strategy"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
+                    Read About How Billing Works
+                </a>
+                <p className={styles.section__text}>
+                    If an interface in one of your projects has the <code>InterfaceOwner</code> billing strategy enabled, you may see requests from other users
+                    here.
+                </p>
             </section>
             <section className={styles.section}>
                 <h2 className={styles.section__title}>Log Out</h2>
